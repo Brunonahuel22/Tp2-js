a0306533ebe11900084960e39d171f161a4c143f
Bruno Cordoba <brunonahuel87@gmail.com>
2023-11-04 20:45:30 -0300

    pido que ingrese la nota al usuario

---
+++ js/ejercicio2.js
@@ -0,0 +1,11 @@
+/* 
+2- Escribir un programa que solicite una nota (número) de 0  a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+
+0-2: Muy deficiente
+3-4: Insuficiente
+5-6: Suficiente
+7: Bien
+8-9: Notable
+10: Sobresaliente */ 
+
+let notaIngresada = parseFloat(prompt("Ingrese a continuacion su nota: "));
