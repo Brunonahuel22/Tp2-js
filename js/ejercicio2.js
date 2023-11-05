45197210b9aca3049658688912b4b678345f7a98
Bruno Cordoba <brunonahuel87@gmail.com>
2023-11-04 21:30:56 -0300

    prettier para dar formato al codigo

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -6,36 +6,36 @@
 5-6: Suficiente
 7: Bien
 8-9: Notable
-10: Sobresaliente */ 
+10: Sobresaliente */
 
 let notaIngresada = parseFloat(prompt("Ingrese a continuacion su nota: "));
 
 switch (notaIngresada) {
-    case 0:
-    case 1:
-    case 2:
-        alert(("Muy Deficiente"));
-        break;
-    case 3:
-    case 4:
-        alert("Insuficiente");
-        break;
-    case 5:
-    case 6:
-        alert(("Insuficiente"));
-        break;
-    case 7:
-        alert("Bien");
-        break;
-    case 8:
-    case 9:
-        alert("Notable");
-        break;
-    case 10:
-        alert("Sobrecaliente");
-        break;
+  case 0:
+  case 1:
+  case 2:
+    alert("Muy Deficiente");
+    break;
+  case 3:
+  case 4:
+    alert("Insuficiente");
+    break;
+  case 5:
+  case 6:
+    alert("Insuficiente");
+    break;
+  case 7:
+    alert("Bien");
+    break;
+  case 8:
+  case 9:
+    alert("Notable");
+    break;
+  case 10:
+    alert("Sobrecaliente");
+    break;
 
-    default:
-        alert("Selecciono una opcion incorrecta");
-        break;
+  default:
+    alert("Selecciono una opcion incorrecta");
+    break;
 }
